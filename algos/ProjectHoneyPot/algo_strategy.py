f28e7eae81f60e75c1b8ed9c3caf0ed71346ea9d
Davis Carlson <daviscarlson00@gmail.com>
2018-10-04 20:18:01 -0700

    Began algo testing for a wall and EMPs

--- algos/ProjectHoneyPot/algo_strategy.py
+++ algos/ProjectHoneyPot/algo_strategy.py
@@ -67,7 +67,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         resources to build and repair the logo before spending them 
         on anything else.
         """
-        self.build_c1_logo(game_state)
+        self.build_that_wall(game_state)
 
         """
         Then build additional defenses.
@@ -77,41 +77,43 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         Finally deploy our information units to attack.
         """
-        self.deploy_attackers(game_state)
 
-    # Here we make the C1 Logo!
-    def build_c1_logo(self, game_state):
-        """
-        We use Filter firewalls because they are cheap
+        if game_state.get_resource(game_state.BITS) >= 12:
+            self.deploy_attackers(game_state)
 
-        First, we build the letter C.
-        """
-        firewall_locations = [[8, 11], [9, 11], [7,10], [7, 9], [7, 8], [8, 7], [9, 7]]
-        for location in firewall_locations:
-            if game_state.can_spawn(FILTER, location):
-                game_state.attempt_spawn(FILTER, location)
-        
-        """
-        Build the number 1.
-        """
-        firewall_locations = [[17, 11], [18, 11], [18, 10], [18, 9], [18, 8], [17, 7], [18, 7], [19,7]]
-        for location in firewall_locations:
-            if game_state.can_spawn(FILTER, location):
-                game_state.attempt_spawn(FILTER, location)
-
-        """
-        Build 3 dots with destructors so it looks neat.
-        """
-        firewall_locations = [[11, 7], [13, 9], [15, 11]]
-        for location in firewall_locations:
-            if game_state.can_spawn(DESTRUCTOR, location):
-                game_state.attempt_spawn(DESTRUCTOR, location)
+    # # Here we make the C1 Logo!
+    # def build_c1_logo(self, game_state):
+    #     """
+    #     We use Filter firewalls because they are cheap
+    #
+    #     First, we build the letter C.
+    #     """
+    #     firewall_locations = [[8, 11], [9, 11], [7,10], [7, 9], [7, 8], [8, 7], [9, 7]]
+    #     for location in firewall_locations:
+    #         if game_state.can_spawn(FILTER, location):
+    #             game_state.attempt_spawn(FILTER, location)
+    #
+    #     """
+    #     Build the number 1.
+    #     """
+    #     firewall_locations = [[17, 11], [18, 11], [18, 10], [18, 9], [18, 8], [17, 7], [18, 7], [19,7]]
+    #     for location in firewall_locations:
+    #         if game_state.can_spawn(FILTER, location):
+    #             game_state.attempt_spawn(FILTER, location)
+    #
+    #     """
+    #     Build 3 dots with destructors so it looks neat.
+    #     """
+    #     firewall_locations = [[11, 7], [13, 9], [15, 11]]
+    #     for location in firewall_locations:
+    #         if game_state.can_spawn(DESTRUCTOR, location):
+    #             game_state.attempt_spawn(DESTRUCTOR, location)
 
     def build_defences(self, game_state):
         """
         First lets protect ourselves a little with destructors:
         """
-        firewall_locations = [[0, 13], [27, 13]]
+        firewall_locations = [[27, 13], [26, 12], [25, 11], [23, 13], [22, 13]]
         for location in firewall_locations:
             if game_state.can_spawn(DESTRUCTOR, location):
                 game_state.attempt_spawn(DESTRUCTOR, location)
@@ -122,7 +124,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         shields decay over time, so shields closer to the action 
         are more effective.
         """
-        firewall_locations = [[3, 11], [4, 11], [5, 11]]
+        firewall_locations = [[3, 12], [2, 13]]
         for location in firewall_locations:
             if game_state.can_spawn(ENCRYPTOR, location):
                 game_state.attempt_spawn(ENCRYPTOR, location)
@@ -149,37 +151,50 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         While we have cores to spend, build a random Encryptor.
         """
-        while game_state.get_resource(game_state.CORES) >= game_state.type_cost(ENCRYPTOR) and len(possible_locations) > 0:
-            # Choose a random location.
-            location_index = random.randint(0, len(possible_locations) - 1)
-            build_location = possible_locations[location_index]
-            """
-            Build it and remove the location since you can't place two 
-            firewalls in the same location.
-            """
-            game_state.attempt_spawn(ENCRYPTOR, build_location)
-            possible_locations.remove(build_location)
+
+        if game_state.turn_number % 2 == 0:
+            while game_state.get_resource(game_state.CORES) >= game_state.type_cost(ENCRYPTOR) and len(possible_locations) > 0:
+                # Choose a random location.
+                location_index = random.randint(0, len(possible_locations) - 1)
+                build_location = possible_locations[location_index]
+                """
+                Build it and remove the location since you can't place two 
+                firewalls in the same location.
+                """
+                game_state.attempt_spawn(ENCRYPTOR, build_location)
+                possible_locations.remove(build_location)
+
+    def build_that_wall(self, game_state):
+        filter_locations = [[2,12],[1,13],[0,13]]
+
+        for i in range(3, 23):
+            new_location = [i, 11]
+            filter_locations.append(new_location)
+
+        for location in filter_locations:
+            if game_state.can_spawn(FILTER, location):
+                game_state.attempt_spawn(FILTER, location)
 
     def deploy_attackers(self, game_state):
         """
-        First lets check if we have 10 bits, if we don't we lets wait for 
+        First lets check if we have 10 bits, if we don't we lets wait for
         a turn where we do.
         """
-        if (game_state.get_resource(game_state.BITS) < 10):
+        if (game_state.get_resource(game_state.BITS) < 12):
             return
-        
+
         """
         First lets deploy an EMP long range unit to destroy firewalls for us.
         """
-        if game_state.can_spawn(EMP, [3, 10]):
-            game_state.attempt_spawn(EMP, [3, 10])
+        if game_state.can_spawn(EMP, [4, 9],3):
+            game_state.attempt_spawn(EMP, [4, 9],3)
 
         """
         Now lets send out 3 Pings to hopefully score, we can spawn multiple 
         information units in the same location.
         """
-        if game_state.can_spawn(PING, [14, 0], 3):
-            game_state.attempt_spawn(PING, [14,0], 3)
+        while game_state.get_resource(game_state.BITS) >= game_state.type_cost(PING) * 3:
+            game_state.attempt_spawn(PING, [14,0])
 
         """
         NOTE: the locations we used above to spawn information units may become 
