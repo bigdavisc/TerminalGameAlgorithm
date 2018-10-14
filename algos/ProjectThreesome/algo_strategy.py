e2024decff5aaf72b1dfb3abc493636101ac75de
Davis Carlson <daviscarlson00@gmail.com>
2018-10-14 12:50:18 -0700

    Finalized code to detect what has been destroyed

--- algos/ProjectThreesome/algo_strategy.py
+++ algos/ProjectThreesome/algo_strategy.py
@@ -21,14 +21,19 @@ board states. Though, we recommended making a copy of the map to preserve
 the actual current map state.
 """
 
-currentDefences = {"DESTRUCTOR": [],
+whatHasBeenBuilt = {"DESTRUCTOR": [],
                    "ENCRYPTORS": [],
                    "FILTERS": []
                    }
 
-lastRoundDefences = {"DESTRUCTOR": [],
+whatShouldBeOnTheBoard = {"DESTRUCTOR": [],
                    "ENCRYPTORS": [],
                    "FILTERS": []
+                   }
+
+damageWeightings = {"LEFT": 0,
+                   "MIDDLE": 0,
+                   "RIGHT": 0
                    }
 
 class AlgoStrategy(gamelib.AlgoCore):
@@ -73,40 +78,60 @@ class AlgoStrategy(gamelib.AlgoCore):
     strategy and can safey be replaced for your custom algo.
     """
     def starter_strategy(self, game_state):
-        global currentDefences
-        global lastRoundDefences
+        global whatHasBeenBuilt
+        global whatShouldBeOnTheBoard
+        global damageWeightings
 
-        f = open("myfile.txt", "w")
+        highestDamage = 0
+        highestDamageSide = ""
+        for i in damageWeightings:
+            if damageWeightings[i] > highestDamage:
+                highestDamageSide = i
+                highestDamage = damageWeightings[i]
 
-        if game_state.turn_number == 0:
-            self.build_that_scratch_post(game_state)
+        if highestDamage >= 3:
+            if highestDamageSide == "LEFT":
+                # build left
+            elif highestDamageSide == "MIDDLE":
+                # build middle
+            elif highestDamageSide == "RIGHT":
+                #build right
+        else:
+            # ------------------BEGINNING OF GAME--------------------
+            if game_state.turn_number == 0:
+                self.build_that_scratch_post(game_state)
+            self.build_that_wall(game_state)
+            self.build_defences(game_state)
+            # -------------------------------------------------------
 
-        self.build_that_wall(game_state)
 
-        self.build_defences(game_state)
+        whatShouldBeOnTheBoard = self.removeDuplicates(whatShouldBeOnTheBoard).copy()
+        destroyedFirewalls = self.find_destroyed_firewalls(whatShouldBeOnTheBoard,whatHasBeenBuilt)
+        self.set_side_weighting(destroyedFirewalls)
+        for key in whatHasBeenBuilt:
+            for xy in whatHasBeenBuilt[key]:
+                whatShouldBeOnTheBoard[key].append(xy)
+        for key in whatHasBeenBuilt:
+            whatHasBeenBuilt[key] = []
 
+        # -----------DEPLOY ATTACKERS EVERY ROUND------------
         self.deploy_attackers(game_state)
-
-        destroyedFirewalls = self.find_destroyed_firewalls(lastRoundDefences,currentDefences)
-
-        f.write(str(currentDefences))
-
-        lastRoundDefences = currentDefences
+        # ---------------------------------------------------
 
     def build_defences(self, game_state):
-        global currentDefences
+        global whatHasBeenBuilt
 
         firewall_locations = [[0,13],[1,12],[27,13],[23,12],[24,13],[20,10],[2,11],[25,11],[13,10],[6,10]]
         for location in firewall_locations:
             if game_state.can_spawn(DESTRUCTOR, location):
                 game_state.attempt_spawn(DESTRUCTOR, location)
-                currentDefences["DESTRUCTOR"].append(location)
+                whatHasBeenBuilt["DESTRUCTOR"].append(location)
 
         firewall_locations = [[26,12], [24,10]]
         for location in firewall_locations:
             if game_state.can_spawn(ENCRYPTOR, location):
                 game_state.attempt_spawn(ENCRYPTOR, location)
-                currentDefences["ENCRYPTORS"].append(location)
+                whatHasBeenBuilt["ENCRYPTORS"].append(location)
 
         destructor_wall = []
         for i in range(3,21):
@@ -115,13 +140,13 @@ class AlgoStrategy(gamelib.AlgoCore):
         for location in destructor_wall:
             if game_state.can_spawn(DESTRUCTOR, location):
                 game_state.attempt_spawn(DESTRUCTOR, location)
-                currentDefences["DESTRUCTOR"].append(location)
+                whatHasBeenBuilt["DESTRUCTOR"].append(location)
 
         firewall_locations = [[23,9]]
         for location in firewall_locations:
             if game_state.can_spawn(DESTRUCTOR, location):
                 game_state.attempt_spawn(DESTRUCTOR, location)
-                currentDefences["DESTRUCTOR"].append(location)
+                whatHasBeenBuilt["DESTRUCTOR"].append(location)
 
         encryptor_wall = []
         for i in range(20, 6, -3):
@@ -130,23 +155,10 @@ class AlgoStrategy(gamelib.AlgoCore):
         for location in encryptor_wall:
             if game_state.can_spawn(ENCRYPTOR, location):
                 game_state.attempt_spawn(ENCRYPTOR, location)
-                currentDefences["ENCRYPTORS"].append(location)
-
-
-
-        # all_locations = []
-        # for i in range(game_state.ARENA_SIZE):
-        #     for j in range(math.floor(game_state.ARENA_SIZE / 2)):
-        #         if (game_state.game_map.in_arena_bounds([i, j])):
-        #             all_locations.append([i, j])
-        #
-        # """
-        # Then we remove locations already occupied.
-        # """
-        # possible_locations = self.filter_blocked_locations(all_locations, game_state)
+                whatHasBeenBuilt["ENCRYPTORS"].append(location)
 
     def build_that_wall(self, game_state):
-        global currentDefences
+        global whatHasBeenBuilt
         filter_locations = []
 
         for i in range(2, 23):
@@ -156,22 +168,22 @@ class AlgoStrategy(gamelib.AlgoCore):
         for location in filter_locations:
             if game_state.can_spawn(FILTER, location):
                 game_state.attempt_spawn(FILTER, location)
-                currentDefences["FILTERS"].append(location)
+                whatHasBeenBuilt["FILTERS"].append(location)
 
     def build_that_scratch_post(self, game_state):
-        global currentDefences
+        global whatHasBeenBuilt
         filter_locations = [[6,11],[7,11],[8,11],[19,11],[20,11],[21,11],[11,11],[12,11],[13,11],[14,11],[15,11],[16,11]]
         destructor_locations = [[7,10], [20,10],[0,13],[1,13],[27,13]]
 
         for location in filter_locations:
             if game_state.can_spawn(FILTER, location):
                 game_state.attempt_spawn(FILTER, location)
-                currentDefences["FILTERS"].append(location)
+                whatHasBeenBuilt["FILTERS"].append(location)
 
         for location in destructor_locations:
             if game_state.can_spawn(DESTRUCTOR, location):
                 game_state.attempt_spawn(DESTRUCTOR, location)
-                currentDefences["DESTRUCTOR"].append(location)
+                whatHasBeenBuilt["DESTRUCTOR"].append(location)
 
     def deploy_attackers(self, game_state):
 
@@ -192,17 +204,39 @@ class AlgoStrategy(gamelib.AlgoCore):
             game_state.attempt_spawn(SCRAMBLER, [5, 8])
 
 
-        
-    def filter_blocked_locations(self, locations, game_state):
-        filtered = []
-        for location in locations:
-            if not game_state.contains_stationary_unit(location):
-                filtered.append(location)
-        return filtered
+    def find_destroyed_firewalls(self, whatsShouldBeTheBoard, whatHasBeenBuilt):
 
-    def find_destroyed_firewalls(self, lastFirewalls, newFirewalls):
+        destroyedDefences = {"DESTRUCTOR": [],
+                           "ENCRYPTORS": [],
+                           "FILTERS": []
+                           }
 
-        return list(set(lastFirewalls) - set(newFirewalls))
+        for key in whatsShouldBeTheBoard:
+            destroyedDefences[key] = [item for item in whatHasBeenBuilt[key] if item in whatsShouldBeTheBoard[key]]
+
+        return destroyedDefences
+
+    def set_side_weighting(self,destroyedDictionary):
+        global damageWeightings
+
+        for key in destroyedDictionary:
+            for xy in destroyedDictionary[key]:
+                if xy[0] in range(0,9): damageWeightings["LEFT"] += 1
+                elif xy[0] in range(9,19): damageWeightings["MIDDLE"] += 1
+                else: damageWeightings["RIGHT"] += 1
+                
+    def removeDuplicates(self,whatHasBeenBuilt):
+
+        output = {"DESTRUCTOR": [],
+                             "ENCRYPTORS": [],
+                             "FILTERS": []
+                             }
+
+        for key in whatHasBeenBuilt:
+            for xy in whatHasBeenBuilt[key]:
+                if xy not in output[key]:
+                    output[key].append(xy)
+        return output
 
 if __name__ == "__main__":
     algo = AlgoStrategy()
